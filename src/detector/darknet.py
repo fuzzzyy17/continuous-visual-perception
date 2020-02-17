f1c80e5afb549aa85843c1d45b1ed0a487ad08ca
Faizan Mohamed <faizan_7-8-6@hotmail.co.uk>
2020-02-17 23:28:06 +0000

    create modules from yolov3 config

--- src/detector/darknet.py
+++ src/detector/darknet.py
@@ -1,10 +1,20 @@
 from __future__ import division
 
+import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 import numpy as np
+
+class EmptyLayer(nn.Module):
+    def __init__(self):
+        super(EmptyLayer, self).__init__()
+
+class DetectionLayer(nn.Module):
+    def __init__(self, anchors):
+        super(DetectionLayer, self).__init__()
+        self.anchors = anchors
 
 def parse_cfg(cfgfile):
 
@@ -18,7 +28,7 @@ def parse_cfg(cfgfile):
     blocks = []
 
     for line in lines:
-        if lines[0] == "[":
+        if line[0] == "[":
             if len(block) != 0:
                 blocks.append(block)
                 block = {}
@@ -29,3 +39,92 @@ def parse_cfg(cfgfile):
     blocks.append(block)
 
     return blocks
+
+def create_modules(blocks): #create module per block 
+    net_info = blocks[0] #info on input/preprocessing
+    module_list = nn.ModuleList()
+    prev_filters = 3
+    output_filters = []
+
+    for index, x in enumerate(blocks[1:]):
+        module = nn.Sequential() #exec module objs sequentially
+
+        if (x["type"] == "convolutional"):
+            activation = x["activation"]
+            try:
+                batch_normalize = int(x["batch_normalize"])
+                bias = False
+            except:
+                batch_normalize = 0
+                bias = True
+
+            filters = int(x["filters"])
+            padding = int(x["pad"])
+            kernel_size = int(x["size"])
+            stride = int(x["stride"])
+
+            if padding:
+                pad = (kernel_size - 1) // 2
+            else:
+                pad = 0
+
+            conv = nn.Conv2d(prev_filters, filters, kernel_size, stride, pad, bias=bias)
+            module.add_module("conv_{0}".format(index), conv) # add conv layer
+
+            if batch_normalize: # add batch norm layer
+                bn = nn.BatchNorm2d(filters)
+                module.add_module("batch_norm_{0}".format(index), bn)
+
+            if activation == "leaky": #check activation func
+                activn = nn.LeakyReLU(0.1, inplace=True)
+                module.add_module("leaky_{0}".format(index), activn)
+
+        elif (x["type"] == "upsample"): #use bilinear2dup if upsamp layer
+            stride = int(x["stride"])
+            upsample = nn.Upsample(scale_factor=2, mode="bilinear")
+            module.add_module("upsample_{0}".format(index), upsample)
+
+        elif (x["type"] == "route"): # if route layer
+            x["layers"] = x["layers"].split(',')
+
+            start = int(x["layers"][0])
+            try: #check for second layer
+                end = int(x["layers"][1])
+            except:
+                end = 0
+            
+            if start > 0:
+                start = start - index
+            if end > 0:
+                end = end - index
+            route = EmptyLayer()
+            module.add_module("route_{0}".format(index), route)
+            if end < 0: # if concating maps
+                filters = output_filters[index + start] + output_filters[index + end]
+            else:
+                filters = output_filters[index + start]
+        
+        elif x["type"] == "shortcut":
+            shortcut = EmptyLayer()
+            module.add_module("shortcut_{0}".format(index), shortcut)
+
+        elif x['type'] == 'yolo':
+            mask = x['mask'].split(',')
+            mask = [int(x) for x in mask]
+
+            anchors = x['anchors'].split(',')
+            anchors = [int(a) for a in anchors]
+            anchors = [(anchors[i], anchors[i+1]) for i in range(0, len(anchors), 2)]
+            anchors = [anchors[i] for i in mask]
+
+            detection = DetectionLayer(anchors)
+            module.add_module("Detection_{0}".format(index), detection)
+
+        module_list.append(module)
+        prev_filters = filters
+        output_filters.append(filters)
+
+    return (net_info, module_list)
+
+blocks = parse_cfg('src/detector/cfg/yolov3.cfg')
+print(create_modules(blocks))
