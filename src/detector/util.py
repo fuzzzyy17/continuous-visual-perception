2ba2d9cf41e1875c0c58f0a4fa04b37904ccea40
Faizan Mohamed <faizan_7-8-6@hotmail.co.uk>
2020-02-24 21:43:40 +0000

    objectness score thresholding, nms started

--- src/detector/util.py
+++ src/detector/util.py
@@ -63,3 +63,53 @@ def transform_pred(pred, input_dim, no_classes, anchors, CUDA):
     pred[:,:,:4] *= stride #resize detection map to size of input img
     
     return pred
+
+def get_img_classes(tensor):
+    numpy_tensor = tensor.cpu().numpy()
+    unique_numpy = np.unique(numpy_tensor)
+    unique_tensor = torch.from_numpy(unique_numpy)
+    tensor_ = tensor.new(unique_tensor.shape)
+    tensor_.copy_(unique_tensor)
+    return tensor_
+
+def results(pred, no_classes, conf, nms_thresh=.4):
+
+    pre_conf = (pred[:,:,4] > conf).float().unsqueeze(2) #check if bbox obj score above thresh
+    pred = pred * pre_conf #if below, =0
+
+    corner_box_coords = pred.new(pred.shape) #move coords from middle to top left corner
+    corner_box_coords[:,:,3] = (pred[:,:,1] - pred[:,:,3]/2)
+    corner_box_coords[:,:,2] = (pred[:,:,0] - pred[:,:,2]/2)
+    corner_box_coords[:,:,1] = (pred[:,:,1] - pred[:,:,3]/2)
+    corner_box_coords[:,:,0] = (pred[:,:,0] - pred[:,:,2]/2)
+    
+    pred[:,:,:4] = corner_box_coords[:,:,:4]
+
+    batch_size = pred.size(0)
+    out_init = False #output not init. 
+
+    for i in range(batch_size): #loop over 1st dim of pred since nms/conf done 1 img at a time
+        image_pred = pred[i]
+        max_conf, max_conf_score = torch.max(image_pred[:,5:5 + no_classes], 1) #get index of 5 highest score/val classes
+        max_conf_score = max_conf_score.float().unsqueeze(1)
+        max_conf = max_conf.float().unsqueeze(1)
+        top = (image_pred[:,:5], max_conf, max_conf_score)
+        image_pred = torch.cat(top, 1)
+        
+        thresh_indxs = (torch.nonzero(image_pred[:,4]))#remove boxes w/ obj conf < thresh
+        if image_pred.shape[0] == 0:
+            continue
+        try:
+            image_pred = image_pred[thresh_indxs.squeeze(),:].view(-1,7)
+        except:
+            continue
+
+        img_classes = get_img_classes(image_pred[:,-1])
+        for class_ in img_classes: #non-max supprs.
+            class_detections = image_pred*(image_pred[:,-1]==class_).float().unsqueeze(1) #find objs of spec. class 
+            class_detections_indxs = torch.nonzero(class_detections[:,-2]).squeeze()
+            img_class_prediction = image_pred[class_detections_indxs].view(-1,7)
+
+            sort_conf = torch.sort(img_class_prediction[:,4], descending=True)[1] #sort by max obj conf
+            img_class_prediction = img_class_prediction[sort_conf]
+            index = img_class_prediction.size(0) #no. detections
